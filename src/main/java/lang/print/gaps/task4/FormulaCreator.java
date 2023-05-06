bcd419ee0b23f30ac3198e9b1302cd6459baf302
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-06 23:13:56 +0200

    Finished.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task4;
 
+import static java.lang.Math.pow;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 2;
+        int b = 10;
+        double sum = ((9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2);
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a = 97;
+        char c = 99;
+        char h = 104;
+        char r = 114;
     }
 }
